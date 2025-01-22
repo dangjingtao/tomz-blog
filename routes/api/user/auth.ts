654974f0d41deaafe8f250c482eadd71b49cc76d
Tomz <dangjingtao@163.com>
2025-01-22 20:57:44 +0800

    debug

--- routes/api/user/auth.ts
+++ routes/api/user/auth.ts
@@ -1,12 +1,12 @@
 // routes/api/example.ts
 import { auth } from "@/utils/user.ts";
-// import kv from "@/lib/kv.ts";
+import kv from "@/lib/kv.ts";
 export const handler = {
   async POST(req, _ctx) {
     const { code } = await req.json();
     const response = await auth(code);
     if (response.status === 200) {
-      // await kv.set(["preferences", "user"], response);
+      await kv.set(["preferences", "user"], response);
     }
     return new Response(JSON.stringify(response), {
       status: response.status,
