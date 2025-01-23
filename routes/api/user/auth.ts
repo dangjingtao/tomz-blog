19bb4f72a1b971a48497afe47f141d34fdb6ccff
Tomz <dangjingtao@163.com>
2025-01-23 18:21:13 +0800

    feat: remove kv

--- routes/api/user/auth.ts
+++ routes/api/user/auth.ts
@@ -1,12 +1,10 @@
 // routes/api/example.ts
 import { auth } from "@/utils/user.ts";
-import kv from "@/lib/kv.ts";
 export const handler = {
   async POST(req, _ctx) {
     const { code } = await req.json();
     const response = await auth(code);
     if (response.status === 200) {
-      await kv.set(["preferences", "user"], response);
     }
     return new Response(JSON.stringify(response), {
       status: response.status,
